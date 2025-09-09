d6a15c1e64d5ef4d98e7531244aadacd03e9df5a
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-09 15:42:15 -0500

    feat(error): add custom error class.

---
+++ src/shared/domain/model/errors.js
@@ -0,0 +1,9 @@
+/**
+ *  Custom error class for validation errors.
+ */
+export class ValidationError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = 'ValidationError';
+    }
+}
